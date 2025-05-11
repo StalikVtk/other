570cf93e5926d20aa5e577b01ec706839d6b645f
StalikVtk <StalikVtk@yandex.ru>
2025-05-11 11:46:22 +0400

    Практическое задание № 4. Вывод фраз считанных из консоли.

---
+++ CSharpEducation.Practice/Task4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string str1, str2;
+
+            Console.Write("Введите строку № 1 ");
+            str1 = Console.ReadLine();
+
+            Console.Write("Введите строку № 2 ");
+            str2 = Console.ReadLine();
+
+            Console.WriteLine($"\n{str1} {str2}");
+            Console.ReadKey();
+        }
+    }
+}
