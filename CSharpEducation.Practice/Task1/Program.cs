bc94b1df15fdd9d02b6064672b5f0951a2499b8c
StalikVtk <StalikVtk@yandex.ru>
2025-05-11 11:33:50 +0400

    Практическое задание № 1. Обьявление переменных.

---
+++ CSharpEducation.Practice/Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            string name;
+            string nameCompany;
+            bool isJob;
+            float weight;
+        }
+    }
+}
