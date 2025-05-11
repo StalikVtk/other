1b75fe96133b69eb56bc037d23d73a7e38c6c111
StalikVtk <StalikVtk@yandex.ru>
2025-05-11 11:39:05 +0400

    Практическое задание № 2. Обьявление переменных и их инициализация

---
+++ CSharpEducation.Practice/Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Text;
+
+namespace Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int myAge = 33;
+            string myName = "Stanislav";
+            float tempOutWindow = 16.3f;
+            bool isWomen = false;
+        }
+    }
+}
