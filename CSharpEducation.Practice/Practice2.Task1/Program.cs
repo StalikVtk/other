20347296a6cf6cd2c8a5f6b0dca42c7e22894d8f
StalikVtk <StalikVtk@yandex.ru>
2025-05-11 12:34:56 +0400

    Практическое занятие 2. Задание 1. Цикл For

---
+++ CSharpEducation.Practice/Practice2.Task1/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace Practice2.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write(i + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
