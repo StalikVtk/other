f799478c6a553349d747e1907edb8c9abd04aeee
StalikVtk <StalikVtk@yandex.ru>
2025-05-11 11:49:08 +0400

    Практическое задание № 5. Вычисление площади круга.

---
+++ CSharpEducation.Practice/Task5/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const double numPi = Math.PI;
+            int rCirle = 15;
+            double sCircle;
+
+            sCircle = numPi * Math.Pow(rCirle, 2);
+
+            Console.WriteLine("Площадь круга с R = 15 составляет: " + Math.Round(sCircle, 2));
+            Console.ReadKey();
+        }
+    }
+}
