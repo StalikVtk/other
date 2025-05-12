ed6894807e72616f8c0714d704ea832ff986eff9
StalikVtk <StalikVtk@yandex.ru>
2025-05-12 15:51:09 +0400

    Практическое занятие 2. Задание 4. Прямоугольный треугольник в Console с слевой стороны.

---
+++ CSharpEducation.Practice/Practice2.Task4/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Practice2.Task4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string symbol = "";
+
+            for (int i = 0; i < 15; i++)
+            {
+                Console.WriteLine(symbol.PadRight(i, '*'));
+            }
+            Console.ReadKey();
+        }
+    }
+}
