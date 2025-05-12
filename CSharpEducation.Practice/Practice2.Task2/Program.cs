1235d694777521f0c1df72e3ed06a2700886f425
StalikVtk <StalikVtk@yandex.ru>
2025-05-12 13:33:23 +0400

    Практическое занятие 2. Задание 2. Перевод метры в километры

---
+++ CSharpEducation.Practice/Practice2.Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Text;
+
+namespace Practice2.Task2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float metre = 5400;
+            float kilometer = metre / 1000;
+
+            Console.Write($"В {metre} м. получается {kilometer} км.\n");
+            Console.ReadKey();
+        }
+    }
+}
