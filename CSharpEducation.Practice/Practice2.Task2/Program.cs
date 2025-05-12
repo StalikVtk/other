377693e3b6219a5e2d1492125a45b1dbfeb603f1
StalikVtk <StalikVtk@yandex.ru>
2025-05-12 13:44:51 +0400

    м/с в км/ч

--- CSharpEducation.Practice/Practice2.Task2/Program.cs
+++ CSharpEducation.Practice/Practice2.Task2/Program.cs
@@ -13,6 +13,12 @@ namespace Practice2.Task2
 
             Console.Write($"В {metre} м. получается {kilometer} км.\n");
             Console.Write($"В {kilometer} км. получается {centimetre} см.\n");
+
+            float metreInSecond = 25;
+            float kilometerInHour = (metreInSecond / 1000) * 3600;
+
+            Console.Write($"{metreInSecond} м/с. равно {kilometerInHour} км/ч.\n");
+
             Console.ReadKey();
         }
     }
