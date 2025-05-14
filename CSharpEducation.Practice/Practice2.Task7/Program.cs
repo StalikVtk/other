a86896ad51eb88c2f302b391d94fe01fef39d637
StalikVtk <StalikVtk@yandex.ru>
2025-05-14 12:54:21 +0400

    Практическое занятие 2. Задание 7. Перевод строки в верхний регистр.

---
+++ CSharpEducation.Practice/Practice2.Task7/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Practice2.Task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string registerString;
+
+            Console.Write("Введите строку: ");
+            registerString = Console.ReadLine().ToUpper();
+
+            Console.WriteLine("Строка в верхнем регистре: " + registerString);
+            Console.ReadKey();
+        }
+    }
+}
