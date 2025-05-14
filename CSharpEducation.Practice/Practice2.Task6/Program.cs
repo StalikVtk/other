aa5ddf9a63d6aa8de74858d10dacfc192a54fdc8
StalikVtk <StalikVtk@yandex.ru>
2025-05-14 12:29:54 +0400

    Практическое занятие 2. Задание 6. Процент вхождения символа в строке.

---
+++ CSharpEducation.Practice/Practice2.Task6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practice2.Task6
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputUser = "Hello word! It's a beautiful day today";
+            string compareString;
+            string inputChar = "d";
+            float percentInString;
+
+            compareString = inputUser.Replace(inputChar, "");
+
+            percentInString = (float)(inputUser.Length - compareString.Length) * 100 / inputUser.Length;
+
+            Console.WriteLine("Процент вхождения символа 'd' в строке: " + inputUser + " = " + Math.Round(percentInString, 2) + "%");
+            Console.ReadKey();
+        }
+    }
+}
