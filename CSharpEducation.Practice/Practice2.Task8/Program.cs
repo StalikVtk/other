525adf1072adf0c1d5350d5f2a1ca09e156ec200
StalikVtk <StalikVtk@yandex.ru>
2025-05-15 18:12:10 +0400

    Практическое занятие 2. Задание 8. Увеличение чисел на 5, если среди них есть два равных.

---
+++ CSharpEducation.Practice/Practice2.Task8/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Practice2.Task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = new int[3];
+
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.Write($"Введите число №{i+1}: ");
+                mas[i] = Convert.ToInt32(Console.ReadLine());
+            }
+
+            if (mas[0] == mas[1] || mas[1] == mas[2] || mas[0] == mas[2])
+            {
+                for (int i = 0; i < mas.Length; i++)
+                {
+                    mas[i] += 5; 
+                }
+            }
+            else
+            {
+                Console.WriteLine("Равных нет");
+            }
+        }
+    }
+}
