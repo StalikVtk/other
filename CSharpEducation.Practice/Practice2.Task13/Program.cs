9efa2c647cef8f2089082a478348b721de8babbb
StalikVtk <StalikVtk@yandex.ru>
2025-05-16 16:22:19 +0400

    Практическое занятие 2. Задание 13. Создание и вывод двумерного массива.

---
+++ CSharpEducation.Practice/Practice2.Task13/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Practice2.Task13
+{
+    internal class Program
+    {
+        private static int[,] mas;
+        static void Main(string[] args)
+        {
+            CreateMas(5, 5);
+            PrintMas();
+            Console.ReadKey();
+        }
+
+       public static void CreateMas(int sizeI, int sizeJ)
+        {
+            mas = new int[sizeI, sizeJ];
+            Random rand = new Random();
+
+            for (int i = 0; i < sizeI; i++)
+            {
+                for (int j = 0; j < sizeJ; j++)
+                {
+                    mas[i, j] = rand.Next(100) - 10;
+                }
+            }
+        }
+        static void PrintMas()
+        {
+            int sizeI = mas.GetUpperBound(0) + 1;
+            int sizeJ = mas.Length / sizeI;
+
+            for (int i = 0; i < sizeI; i++)
+            {
+                for (int j = 0; j < sizeJ; j++)
+                {
+                    Console.Write(mas[i, j]+"\t");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
