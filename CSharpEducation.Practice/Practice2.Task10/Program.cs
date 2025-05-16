fdfce176c41313dc64ca53d1d55a2d4bdb588070
StalikVtk <StalikVtk@yandex.ru>
2025-05-16 14:28:48 +0400

    Практическое занятие 2. Задание 10. Создание и вывод массива на экран консоли. Размер массива задается в аргументах командной строки.

---
+++ CSharpEducation.Practice/Practice2.Task10/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Practice2.Task10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size =int.Parse(args[0]);
+
+            int[] mas = new int[size];
+            Random rand = new Random();
+
+            for (int i = 0; i < size; i++)
+            {
+                mas[i] = rand.Next(10);
+                Console.Write(mas[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
