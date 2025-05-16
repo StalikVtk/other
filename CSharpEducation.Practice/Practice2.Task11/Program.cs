179c3fcbf42bc863783035f9132a6c4df38935b1
StalikVtk <StalikVtk@yandex.ru>
2025-05-16 14:38:03 +0400

    Практическое занятие 2. Задание 11.  Создание и вывод массива на консоль. Размер и символ для вставки задается в аргументах командной строки.

---
+++ CSharpEducation.Practice/Practice2.Task11/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Practice2.Task11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = int.Parse(args[0]);
+            int numberInsert = int.Parse(args[1]);
+            int[] mas = new int[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                mas[i] = numberInsert;
+                Console.Write(mas[i]+" ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
