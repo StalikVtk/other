5062b505a852492405b6c7e7c9d8f44b08a03e88
StalikVtk <StalikVtk@yandex.ru>
2025-05-16 15:13:46 +0400

    Практическое занятие 2. Задание 12. Создание массива строк, размер и элементы массива задаются пользователем через консоль.

---
+++ CSharpEducation.Practice/Practice2.Task12/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace Practice2.Task12
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Укажите количество вводимых строк: ");
+            int size =Convert.ToInt32(Console.ReadLine());
+            string[] masString = new string[size];
+
+            for (int i = 0; i < size; i++)
+            {
+                Console.Write($"Введите строку №{i+1} ");
+                masString[i] = Console.ReadLine();
+            }
+            string printString = string.Join(" ", masString);
+            
+            Console.Write(printString);
+            Console.ReadKey();
+        }
+    }
+}
