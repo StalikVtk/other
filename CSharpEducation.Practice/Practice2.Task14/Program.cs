216166f8d3734b258fcb94d13c83bba41a9aa11f
StalikVtk <StalikVtk@yandex.ru>
2025-05-16 16:42:19 +0400

    Практическое занятие 2. Задание 14. Создание массива в методе, размер массива передается через аргумент метода.

---
+++ CSharpEducation.Practice/Practice2.Task14/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Practice2.Task14
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = 10;
+            int[] mas = CreateMas(size);
+            Random rand = new Random();
+
+            for (int i = 0; i < size; i++)
+            {
+                mas[i] = rand.Next(5) - 2;
+                Console.Write(mas[i] + "\t");
+            }
+        }
+        static int[] CreateMas(int n)
+        {
+            int[] mas = new int[n];
+            return mas;
+        }
+    }
+}
