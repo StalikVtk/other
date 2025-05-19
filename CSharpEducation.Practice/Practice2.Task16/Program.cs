d13cc23056574aa52b643c5e146323c65292b485
StalikVtk <StalikVtk@yandex.ru>
2025-05-19 20:57:14 +0400

    Практическое занятие 2. Задание 16. Создание метода который принимает число по ссылке и меняет знак аргумента на противоположный.

---
+++ CSharpEducation.Practice/Practice2.Task16/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Practice2.Task16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = -5;
+            SignNumber(ref a);
+        }
+        static void SignNumber(ref int a)
+        {
+            a = -a;
+        }
+    }
+}
