50ba8650349353214ca2f19e3995884dcb78641c
StalikVtk <StalikVtk@yandex.ru>
2025-05-19 20:25:09 +0400

    Практическое занятие 2. Задание 15. Создание, инвертирование массива и вывод его в консоль.

---
+++ CSharpEducation.Practice/Practice2.Task15/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Practice2.Task15
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = new int[8]{ 1, 2, 3, 4, 5, 6, 7, 8 };
+            ReverseMas(mas);
+        }
+        static void ReverseMas(int[] mas)
+        {
+            Array.Reverse(mas);
+            for (int i = 0; i < mas.Length; i++)
+            {
+                Console.Write(mas[i]+"\t");
+            }
+        }
+    }
+}
