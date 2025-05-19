ae9b37d236999872b2ea5208b370462427d0d137
StalikVtk <StalikVtk@yandex.ru>
2025-05-19 21:08:10 +0400

    Практическое занятие 2. Задание 17. Создание метода который будет менять значения аргументов местами.

---
+++ CSharpEducation.Practice/Practice2.Task17/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Practice2.Task17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a = 5; 
+            int b = 10;
+
+            SwapNumber(a, b);
+        }
+        static void SwapNumber(int a, int b)
+        {
+            int tmp;
+
+            tmp = a;
+            a = b;
+            b = tmp;
+        }
+    }
+}
