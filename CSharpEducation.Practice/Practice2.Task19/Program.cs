ac4bd9ccbdbc5939eb157115577a002f5b12df4d
StalikVtk <StalikVtk@yandex.ru>
2025-05-22 00:05:56 +0400

    Практическое занятие 2. Задание 19. Создание одномерного массива целых чисел и вывод разницы между максимальным и минимальным значением элемента массива.

---
+++ CSharpEducation.Practice/Practice2.Task19/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using Cs = System.Console;
+
+namespace Practice2.Task19
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = new int[10];
+            Random rand = new Random();
+
+            for (int i = 0; i < 10; i++)
+            {
+                mas[i] = rand.Next(99);
+            }
+            Array.Sort(mas);
+            foreach (int arr in mas)
+            {
+                Cs.Write(arr+"\t");
+            }
+            Cs.WriteLine("\nРазница между максимальным и минимальным значением элемента массива: " + (mas[mas.Length - 1] - mas[0]));
+            Cs.ReadKey();
+        }
+    }
+}
