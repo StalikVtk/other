69c15b0ea81945a0a7de36b5ef0df1d54a468cce
StalikVtk <StalikVtk@yandex.ru>
2025-05-21 23:30:45 +0400

    Практическое занятие 2. Задание 18. Создание метода который принимает массив и удаляет из него отрицательные числа. Метод через out параметр передает обратно новый массив и количество удаленных элементов.

---
+++ CSharpEducation.Practice/Practice2.Task18/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Practice2.Task18
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] mas = new int[10] { 1, 5, -4, 9, 2, -6, 7, -3, 8, -1 };
+
+            DeleteNumberIsMas(mas, out int[] newMas, out int countElementDelete);
+
+            Console.Write("Массив после обработки:\n");
+            foreach (int arr in newMas)
+            {
+                Console.Write(arr+"\t");
+            }
+            Console.WriteLine("\nКоличество удаленных элементов из массива: " + countElementDelete);
+            Console.ReadKey();
+        }
+
+        static void DeleteNumberIsMas(int[] mas, out int[] newMas,out int countElementDelete)
+        {
+            newMas = Array.FindAll(mas, element => element > 0);
+            countElementDelete = mas.Length - newMas.Length;
+        }
+    }
+}
