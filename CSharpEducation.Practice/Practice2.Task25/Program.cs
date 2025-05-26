71e49e43d8c1fb606ec67e23a82b594ac24ac6ee
StalikVtk <StalikVtk@yandex.ru>
2025-05-26 17:11:11 +0400

    Практическое занятие 2. Задание 25. Создание перечисления для дней недели. Вывод в консоли наименования дня недели в зависимости от введенного пользователем числа от 1 до 7.

---
+++ CSharpEducation.Practice/Practice2.Task25/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace Practice2.Task25
+{
+    internal class Program
+    {
+        public enum WeekDay
+        {
+            Понедельник = 1,
+            Вторник,
+            Среда,
+            Четверг,
+            Пятница,
+            Суббота,
+            Воскресенье
+        }
+        static void Main(string[] args)
+        {
+            int numberDay;
+            bool correctInputUser = true;
+            while (correctInputUser) 
+            {
+                Console.Write("Введите номер дня недели. Число от 1 до 7: ");
+                numberDay = Convert.ToInt32(Console.ReadLine());
+                if (numberDay < 8 && numberDay > 0)
+                {
+                    var days = (WeekDay)numberDay;
+                    correctInputUser = false;
+                    Console.WriteLine($"Указанное число {numberDay} это {days}");
+                }
+                else
+                {
+                    Console.Clear();
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
