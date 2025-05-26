30b0771f6613b36ecc94bd3415f79c76f59f84e2
StalikVtk <StalikVtk@yandex.ru>
2025-05-26 16:04:23 +0400

    Практическое занятие 2. Задание 23. Вычисление является ли введенный год високосным.

---
+++ CSharpEducation.Practice/Practice2.Task23/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Practice2.Task23
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int year;
+            string message = "";
+            Console.WriteLine("Введите год для определения, будет он являться высокостным или нет.");
+            Console.Write("Год: ");
+            year =Convert.ToInt32(Console.ReadLine());
+
+            if ((year % 4 == 0 && year % 100 != 0) || (year % 400 ==0))
+            {
+                message = "Год высокостный";
+            }
+            else
+            {
+                message = "Год не высокостный";
+            }
+            Console.WriteLine(message);
+            Console.ReadKey();
+        }
+    }
+}
