8fab3fb6e67a5a7b784fe454712067cbbb7392ce
StalikVtk <StalikVtk@yandex.ru>
2025-05-26 16:37:46 +0400

    Практическое занятие 2. Задание 24. Создание перечисления для дней недели. Вывод дней недели на эран.

---
+++ CSharpEducation.Practice/Practice2.Task24/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Practice2.Task24
+{
+    internal class Program
+    {
+        public enum WeekDay
+        {
+            Понедельник,
+            Вторник,
+            Среда,
+            Четверг,
+            Пятница,
+            Суббота,
+            Воскресенье
+        }
+        static void Main(string[] args)
+        {
+            foreach (WeekDay day in Enum.GetValues(typeof(WeekDay)))
+            {
+                Console.WriteLine(day);
+            }
+            Console.ReadKey();
+        }
+    }
+}
