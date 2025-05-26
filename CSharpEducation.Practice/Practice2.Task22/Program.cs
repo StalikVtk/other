af695eb582fef69d0fd52768ad2a3fec58d86440
StalikVtk <StalikVtk@yandex.ru>
2025-05-26 13:42:39 +0400

    Практическое занятие 2. Задание 22. Игра угадай число от 1 до 100. С выводом количества попыток ввода пользователем.

---
+++ CSharpEducation.Practice/Practice2.Task22/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Practice2.Task22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int number = rand.Next(1, 101);
+            int countInputUser = 1;
+            string message = "";
+            int userInput;
+            bool game = true;
+            while (game)
+            {
+                Console.Clear();
+                Console.WriteLine("Мы загадали число от 1 до 100, попробуйте его отгадать.");
+                message = $"Попытка № {countInputUser}";
+                Console.Write(message + " Ваше число: ");
+                userInput = Convert.ToInt32(Console.ReadLine());
+                countInputUser++;
+
+                if (number == userInput)
+                {
+                    game = false;
+                    message = "Вы отгадали. Поздравляем!";
+                }
+            }
+            Console.Clear();
+            Console.WriteLine(message);
+            Console.ReadKey();
+        }
+    }
+}
