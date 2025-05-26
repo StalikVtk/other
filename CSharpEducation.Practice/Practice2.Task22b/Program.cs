7dad629d9663e9c5a1ca2f3b6d1367299bcb9058
StalikVtk <StalikVtk@yandex.ru>
2025-05-26 14:08:58 +0400

    Практическое занятие 2. Задание 22. Игра угадай число от 1 до 100.  Добавление подсказки, заданное число больше или меньше введенного пользователем.

---
+++ CSharpEducation.Practice/Practice2.Task22b/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Practice2.Task22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int number = rand.Next(1, 101);
+            const int numberOfInput = 5;
+            int countInputUser = 1;
+            string message = "";
+            int userInput;
+            bool game = true;
+            while (game)
+            {
+                if (numberOfInput - countInputUser < 0)
+                {
+                    game = false;
+                    message = "Вы проиграли!";
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Мы загадали число от 1 до 100, попробуйте его отгадать." + number);
+                    message = $"Попытка № {countInputUser} из {numberOfInput}";
+                    Console.Write(message + " Ваше число: ");
+                    userInput = Convert.ToInt32(Console.ReadLine());
+                    if (number == userInput)
+                    {
+                        game = false;
+                        message = "Вы отгадали. Поздравляем!";
+                    }
+                    else 
+                    {
+                        Console.WriteLine(number > userInput ? $"Загаданное число больше" : $"Загаданное число меньше");
+                        Console.ReadKey();
+                    }
+                    countInputUser++;
+                }
+            }
+            Console.Clear();
+            Console.WriteLine(message);
+            Console.ReadKey();
+        }
+    }
+}
