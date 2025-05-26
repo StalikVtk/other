9f873f0a6cc1e7a4288defeac01af7f73c0f0bbf
StalikVtk <StalikVtk@yandex.ru>
2025-05-26 13:46:47 +0400

    Практическое занятие 2. Задание 22. Игра угадай число от 1 до 100.  Добавление ограничений по попыткам.

---
+++ CSharpEducation.Practice/Practice2.Task22a/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Practice2.Task22
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int number = rand.Next(1, 101);
+            const int numberOfInput = 5;
+            int countInputUser = 1;
+            string message = "";
+            int userInput;
+            bool game = true;
+            while (game)
+            {
+                if (numberOfInput - countInputUser < 0)
+                {
+                    game = false;
+                    message = "Вы проиграли!";
+                }
+                else
+                {
+                    Console.Clear();
+                    Console.WriteLine("Мы загадали число от 1 до 100, попробуйте его отгадать.");
+                    message = $"Попытка № {countInputUser} из {numberOfInput}";
+                    Console.Write(message + " Ваше число: ");
+                    userInput = Convert.ToInt32(Console.ReadLine());
+                    if (number == userInput)
+                    {
+                        game = false;
+                        message = "Вы отгадали. Поздравляем!";
+                    }
+                    countInputUser++;
+                }
+            }
+            Console.Clear();
+            Console.WriteLine(message);
+            Console.ReadKey();
+        }
+    }
+}
