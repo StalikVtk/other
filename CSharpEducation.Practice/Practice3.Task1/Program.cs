deb4f9e9baa831242426a3d4d48aa11f2d4742a1
StalikVtk <StalikVtk@yandex.ru>
2025-05-26 17:31:35 +0400

    Практическое занятие 3. Задание 1. Создание класса "Student" с полями "Имя", "Возраст". Вывод информации о студенте с помощью метода.

---
+++ CSharpEducation.Practice/Practice3.Task1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Practice3.Task1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student tima = new Student();
+            tima.PrintStudent();
+        }
+    }
+}
---
+++ CSharpEducation.Practice/Practice3.Task1/Student.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Practice3.Task1
+{
+    public class Student
+    {
+        public string name = "Тимофей";
+        public int age = 21;
+
+        public void PrintStudent()
+        {
+            Console.WriteLine("Имя студента: {0}. Возраст: {1} год." ,name,age); 
+        }
+    }
+}
