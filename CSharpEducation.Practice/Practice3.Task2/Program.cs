5e326b0116fc30de1a549ebc57e5c02789d4a726
StalikVtk <StalikVtk@yandex.ru>
2025-05-26 17:59:52 +0400

    Практическое занятие 3. Задание 2. Добавление дополнительного поля "Средний балл" к классу "Student". Добавление свойств к новому полю и проверка установки значения среднего балла.

---
+++ CSharpEducation.Practice/Practice3.Task2/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Practice3.Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student tima = new Student();
+            tima.AverageScoreValue = 4.9;
+
+            tima.PrintStudent();
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharpEducation.Practice/Practice3.Task2/Student.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Practice3.Task2
+{
+    internal class Student
+    {
+        public string name = "Тимофей";
+        public int age = 21;
+        private double averageScoreValue = 4.8;
+
+        public double AverageScoreValue
+        {
+            get 
+            { 
+                return averageScoreValue; 
+            }
+            set 
+            {
+                if ((value > 0) && (value < 5))
+                {
+                   averageScoreValue = value;
+                }
+            }
+        }
+
+        public void PrintStudent()
+        {
+            Console.WriteLine($"Имя студента: {name}. Возраст: {age}. Средний бал: {averageScoreValue}.");
+        }
+    }
+}
