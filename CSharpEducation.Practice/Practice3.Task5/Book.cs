eff6c0a645b1fdae12636634d914c38d00e6677e
StalikVtk <StalikVtk@yandex.ru>
2025-05-29 13:31:32 +0400

    Практическое занятие 3. Задание 5. Создание класса Книга с полями Названияе, автор. Расширение класса спомощью поля Год издания книги. Реализация цепочки конструкторов класса.

---
+++ CSharpEducation.Practice/Practice3.Task5/Book.cs
@@ -0,0 +1,26 @@
+﻿namespace Practice3.Task4
+{
+    internal class Book
+    {
+        public string name { get; }
+        public string author { get; }
+        public string yearCreateBook { get; }
+
+        public Book(string name)
+        {
+            this.name = name;
+        }
+        public Book(string name, string author)
+        {
+            this.name = name;
+            this.author = author;
+        }
+        public Book(string name, string author, string year)
+        {
+            this.name = name;
+            this.author = author;
+            this.yearCreateBook = year;
+        }
+
+    }
+}
---
+++ CSharpEducation.Practice/Practice3.Task5/Program.cs
@@ -0,0 +1,23 @@
+﻿using Practice3.Task4;
+using System;
+using System.Net.Http.Headers;
+
+namespace Practice3.Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book("Книга 1");
+            Console.WriteLine("- " + book1.name);
+
+            Book book2 = new Book("Книга 2", "Автор");
+            Console.WriteLine("- " + book2.name + " " + book2.author);
+
+            Book book3 = new Book("Книга 3", "Автор", "2025 г.");
+            Console.WriteLine("- " + book3.name + " " + book3.author +" "+book3.yearCreateBook);
+
+            Console.ReadKey();
+        }
+    }
+}
