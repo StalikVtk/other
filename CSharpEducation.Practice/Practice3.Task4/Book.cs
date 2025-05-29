f92208c02ad45ee4832f7fcee8c23b0f82a7d566
StalikVtk <StalikVtk@yandex.ru>
2025-05-29 13:16:01 +0400

    Практическое занятие 3. Задание 4. Создание класса Книга с полями Названияе, автор. Реализация двух конструкторов класса.

---
+++ CSharpEducation.Practice/Practice3.Task4/Book.cs
@@ -0,0 +1,19 @@
+﻿namespace Practice3.Task4
+{
+    internal class Book
+    {
+        public string name { get; }
+        public string author { get; }
+
+        public Book(string name, string author)
+        {
+            this.name = name;
+            this.author = author;
+        }
+        public Book()
+        {
+            this.name = "Программирование на С#";
+            this.author = "Васильев А.В.";
+        }
+    }
+}
---
+++ CSharpEducation.Practice/Practice3.Task4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Practice3.Task4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book1 = new Book();
+            Console.WriteLine("- " + book1.name + " " + book1.author);
+
+            Book book2 = new Book("CLR via C#", "Джеффри Рихтер");
+            Console.WriteLine("- " + book2.name + " " + book2.author);
+        }
+    }
+}
