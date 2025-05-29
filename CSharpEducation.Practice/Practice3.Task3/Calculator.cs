c744fba5b404ea4fbfb63f8253a5e030bda2ebfd
StalikVtk <StalikVtk@yandex.ru>
2025-05-29 13:00:54 +0400

    Практическое занятие 3. Задание 3. Создание класса калькулятор.

---
+++ CSharpEducation.Practice/Practice3.Task3/Calculator.cs
@@ -0,0 +1,22 @@
+﻿namespace Practice3.Task3
+{
+    public class Calculator
+    {
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Subtrcation(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multiply(int a, int b)
+        {
+            return a * b;
+        }
+        public static double Divide(int a, int b)
+        {
+            return (double)a / b;
+        }
+    }
+}
---
+++ CSharpEducation.Practice/Practice3.Task3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Practice3.Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Результат сложения чисел 5 и 10 равен " + Calculator.Add(5, 10));
+            Console.WriteLine("Результат вычитания чисел 17 и 25 равен " + Calculator.Subtrcation(17, 25));
+            Console.WriteLine("Результат умножения чисел 13 и 6 равен " + Calculator.Multiply(13, 6));
+            Console.WriteLine("Результат деления чисел 45 и 4 равен " + Calculator.Divide(45, 4));
+            
+            Console.ReadKey();
+        }
+    }
+}
