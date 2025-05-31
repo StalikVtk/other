324015970e2ff0cf1b0ca350e544f75f8254541b
StalikVtk <StalikVtk@yandex.ru>
2025-05-31 09:50:20 +0400

    Практическое занятие 3. Задание 7. Создание перечисления "Месяцы" с элементами представляющие названия месяцев года. Создание метода который возвращает количество дней в указанном пользователем месяце.

---
+++ CSharpEducation.Practice/Practice3.Task7/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+namespace Practice3.Task7
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите название месяца: ");
+            string nameMonth = Console.ReadLine().ToLower();
+            DateTime year = DateTime.Now;
+
+            foreach (int numberMonth in Enum.GetValues(typeof(Month)))
+            {
+                string name = Enum.GetName(typeof(Month), numberMonth);
+                if (nameMonth == name)
+                {
+                    Console.WriteLine("В указанном месяце количество дней равно: " + DateTime.DaysInMonth(year.Year, numberMonth));
+                }
+                else
+                {
+                    Console.WriteLine("Ошибка! Месяц введен некорректно!");
+                    break;
+                }
+            }
+            Console.ReadLine();
+        }
+        public enum Month
+        {
+            январь = 1,
+            февраль,
+            март,
+            апрель,
+            май,
+            июнь,
+            июль,
+            август,
+            сентябрь,
+            октябрь,
+            ноябрь,
+            декабрь
+        }
+    }
+}
