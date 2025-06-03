f11c8bd7d96ce72885c7681ff3a9ba30fbf60cc2
StalikVtk <StalikVtk@yandex.ru>
2025-06-03 23:45:44 +0400

    Практическое занятие 3. Задание 8. Создать структуру прямоугольник с полями "ширина", "высота". Создать метод который будет вычислять площадь прямоугольника по заданным размерам.

---
+++ CSharpEducation.Practice/Practice3.Task8/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Practice3.Task8
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Введите ширину прямоугольника: ");
+            double width = Convert.ToDouble(Console.ReadLine());
+
+            Console.Write("Введите высоту прямоугольника: ");
+            double height = Convert.ToDouble(Console.ReadLine());
+
+            Rectangle rectangle = new Rectangle(width, height);
+
+            Console.WriteLine("\nПлощадь прямоугольника: " + Rectangle.SquareRectangle(rectangle));
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CSharpEducation.Practice/Practice3.Task8/Rectangle.cs
@@ -0,0 +1,17 @@
+﻿namespace Practice3.Task8
+{
+    public struct Rectangle
+    {
+        public double width { get; }
+        public double height { get; }
+        public Rectangle(double width, double height)
+        {
+            this.width = width;
+            this.height = height;
+        }
+        public static double SquareRectangle(Rectangle rect)
+        {
+            return rect.width * rect.height;
+        }
+    }
+}
