95aa51544c648e56172b3ccc2f2afe8129228f0f
StalikVtk <StalikVtk@yandex.ru>
2025-06-05 21:59:31 +0400

    Практическое занятие 3. Задание 11. Создание класса "Книга" с полями "Название", "Автор". Реализация метода, который принимает обьект класса "Книга" и выводит в консоль инфорацию о книге в виде строки.

---
+++ CSharpEducation.Practice/Practice3.Task11/Book.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Practice3.Task11
+{
+    internal class Book
+    {
+        public string name;
+        public string author;
+        public Book(string name, string author)
+        {
+            this.name = name;
+            this.author = author;
+        }
+        public static string PrintBook(Book book, out string outStr)
+        {
+            return outStr = "Название книги: " + book.name + ". Автор книги: " + book.author + ".";
+        }
+    }
+}
---
+++ CSharpEducation.Practice/Practice3.Task11/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Practice3.Task11
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Book book = new Book("Оно", "Стивен Кинг");
+
+            Console.WriteLine(Book.PrintBook(book, out string infoBook));
+            Console.ReadKey();
+        }
+    }
+}
