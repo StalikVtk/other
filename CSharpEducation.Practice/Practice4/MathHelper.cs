c1167c8e98388a3ca18f4b722a0a86668a68002d
StalikVtk <StalikVtk@yandex.ru>
2025-06-05 22:53:12 +0400

    Практическое занятие 4. Задание 1. Создание библиотеки классов, в которой будет класс "MathHelper" с методами для выполнения основных математических операций

---
+++ CSharpEducation.Practice/Practice4/MathHelper.cs
@@ -0,0 +1,26 @@
+﻿namespace Practice4.Task1
+{
+    public class MathHelper
+    {
+        public static int AddNumber(int a, int b)
+        {
+            return a + b;
+        }
+        public static int DoubleNumber(int a, int b)
+        {
+            return a * b;
+        }
+        public static int SubtractNumber(int a, int b)
+        {
+            return a - b;
+        }
+        public static double DivNumber(int a, int b)
+        {
+            if (b == 0)
+            {
+                return 0;
+            }
+            return a / b;
+        }
+    }
+}
