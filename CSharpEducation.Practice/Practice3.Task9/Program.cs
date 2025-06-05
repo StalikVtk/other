5f9e274f06bf8a4c2014c8f29bca32ab91434eeb
StalikVtk <StalikVtk@yandex.ru>
2025-06-05 21:22:33 +0400

    Практическое занятие 3. Задание 9. Класс "Студент" с полями имя и возраст. Метод принимающий обьект класса студент и меняющий имя студента на "Аноним"

---
+++ CSharpEducation.Practice/Practice3.Task9/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Practice3.Task9
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student student = new Student("Арсений", 22);
+
+            Student.UpdateNameStudent(student);
+        }
+    }
+}
---
+++ CSharpEducation.Practice/Practice3.Task9/Student.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Practice3.Task9
+{
+    internal class Student
+    {
+        public string name { get; set; }
+        public int age { get; set; }
+        public Student(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+        public static void UpdateNameStudent(Student student)
+        {
+            student.name = "Аноним";
+        }
+    }
+}
