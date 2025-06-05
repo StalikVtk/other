8d74bc5a951e2d3e297b6ce36a7a9c0b9423400d
StalikVtk <StalikVtk@yandex.ru>
2025-06-05 23:03:35 +0400

    Практическое занятие 4. Задание 2. Создание нового проекта и подключения к нему библиотеки "MathHelper". Использование методов библиотеки в классе программ.

---
+++ CSharpEducation.Practice/Practice4.Task2/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using Practice4.Task1;
+
+namespace Practice4.Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Метод сложения из библиотеки MathHelper " + MathHelper.AddNumber(3, 4));
+            Console.WriteLine("Метод вычитания из библиотеки MathHelper " + MathHelper.SubtractNumber(8, 4));
+            Console.WriteLine("Метод умножения из библиотеки MathHelper " + MathHelper.DoubleNumber(3, 4));
+            Console.WriteLine("Метод деления из библиотеки MathHelper " + MathHelper.DivNumber(12 ,3));
+            Console.ReadKey();
+        }
+    }
+}
