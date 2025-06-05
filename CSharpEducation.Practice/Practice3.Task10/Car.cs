b00ba20615aec4a5403eb627b6519fd33e04a521
StalikVtk <StalikVtk@yandex.ru>
2025-06-05 21:41:17 +0400

    Практическое занятие 3. Задание 10. Создание класса "Автомобиль" с полем "Марка". Написать метод, который будет принимать обьект класса "Автомобиль" и выведет марку автомобиля в консоль.

---
+++ CSharpEducation.Practice/Practice3.Task10/Car.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace Practice3.Task10
+{
+    internal class Car
+    {
+        public string mark;
+        public Car(string mark)
+        {
+            this.mark = mark;
+        }
+        public static void PrintMark(Car car)
+        {
+            Console.WriteLine("Марка автомобиля: " + car.mark);
+        }
+    }
+}
---
+++ CSharpEducation.Practice/Practice3.Task10/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Practice3.Task10
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car car = new Car("Honda");
+
+            Car.PrintMark(car);
+            Console.ReadKey();
+        }
+    }
+}
