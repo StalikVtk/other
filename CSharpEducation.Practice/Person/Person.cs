8dbc6a1b2d439879c64162bfc57b700766c39eea
StalikVtk <StalikVtk@yandex.ru>
2025-06-08 00:33:36 +0400

    Практическое занятие 4. Задание 3. Создание библиотеки с классом "Person" с полями "Имя" и "Возраст". Создание нового проекта и подключение к нему библиотеки с классом "Person". Создание обьектов класса и вывод информации о сотрудниках в консоль в классе Program.

---
+++ CSharpEducation.Practice/Person/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace PersonLib
+{
+    public class Person
+    {
+        private string _personName;
+        private int _personAge;
+
+        public Person() { }
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+        public string Name 
+        {
+            get { return _personName; }
+            set
+            {
+                if ((value is null) || value.Length == 0)
+                {
+                    throw new ArgumentNullException("Имя не может быть пустым!");
+                }
+                else
+                {
+                    _personName = value;
+                }
+            }
+        }
+        public int Age
+        {
+            get { return _personAge; }
+            set 
+            {
+                if (value < 0)
+                {
+                    throw new ArgumentOutOfRangeException("Недопустимый возраст! Он не может быть меньше нуля!");
+                }
+                else if (value > 100)
+                { 
+                    throw new ArgumentOutOfRangeException("Недопустимый возраст! Он не может быть больше чем сто!!!");
+                }
+                else
+                {
+                    _personAge = value;
+                }
+            }
+        }
+        public void PrintInfoPerson()
+        {
+            Console.WriteLine("Имя: " + _personName);
+            Console.WriteLine("Возраст: " + _personAge);
+        }
+    }
+}
---
+++ CSharpEducation.Practice/Practice4.Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿using PersonLib;
+using System;
+
+namespace Practice4.Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Person vlad = new Person("Владислав", 34);
+            Person ivan = new Person("Иван", 38);
+
+            vlad.PrintInfoPerson();
+            ivan.PrintInfoPerson();
+
+            Console.ReadKey();
+        }
+    }
+}
