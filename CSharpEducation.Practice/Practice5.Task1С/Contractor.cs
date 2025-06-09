ff72de2af4ab4cea1f612a6f9f4661a86dd7670e
StalikVtk <StalikVtk@yandex.ru>
2025-06-09 15:24:51 +0400

    Практическое занятие 5. Задание 1C. Создание класса "Contractor" c полем "HourlyRate" наследованный от класса "Employee". Создание метода расчета бонуса в зависмости от отработанных часов. Создание списка сотрудников и расчет бонуса для каждого сотрудника.

---
+++ CSharpEducation.Practice/Practice5.Task1С/Contractor.cs
@@ -0,0 +1,29 @@
+﻿using Practice5.Task1C;
+using System;
+
+namespace Practice5.Task1С
+{
+    internal class Contractor : Employee
+    {
+        double _hourlyRate;
+        public Contractor(string name, double hourlyRate) : base(name, 0)
+        {
+            Name = name;
+            HourlyRate = hourlyRate;
+        }
+        public double HourlyRate
+        {
+            get { return _hourlyRate; }
+            set
+            {
+                if (value < 1)
+                    throw new ArgumentOutOfRangeException("Часовая ставка не может быть меньше 1 руб.");
+                _hourlyRate = value;
+            }
+        }
+        public new double CalculateBonus(int hoursWorked)
+        {
+            return HourlyRate * hoursWorked * 0.3;
+        }
+    }
+}
---
+++ CSharpEducation.Practice/Practice5.Task1С/Program.cs
@@ -0,0 +1,42 @@
+﻿using Practice5.Task1C;
+using System;
+using System.CodeDom;
+using System.Collections.Generic;
+
+namespace Practice5.Task1С
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Employee> employees = new List<Employee>
+            {
+                new Employee("Рядовой сотрудник №1", 45000),
+                new Manager("Менеджер №1", 50000, 7),
+                new Contractor("Контрактный сотрудник №1", 250.5),
+                new Employee("Рядовой сотрудник №2", 40000),
+                new Contractor("Контрактный сотрудник №2", 250.5),
+                new Manager("Менеджер №2", 50000, 5),
+                new Contractor("Контрактный сотрудник №3", 260.5)
+            };
+            foreach (var employee in employees)
+            {
+                if (employee is Contractor contractor)
+                {
+                    Console.Write("Укажите количество отработанных часов для " + contractor.Name + ": ");
+                    int hours = Convert.ToInt32(Console.ReadLine());
+                    Console.WriteLine("Бонус для " + contractor.Name + " состовляет: " + contractor.CalculateBonus(hours));
+                }
+                else if (employee is Manager manager)
+                {
+                    Console.WriteLine("Бонус для " + manager.Name + " состовляет: " + manager.CalculateBonus());
+                }
+                else
+                {
+                    Console.WriteLine("Бонус для " + employee.Name + " состовляет: " + employee.CalculateBonus());
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
