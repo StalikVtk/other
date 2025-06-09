dc2c16cf6109da90fdc56fd29331b3f719d889a0
StalikVtk <StalikVtk@yandex.ru>
2025-06-09 10:13:14 +0400

    Практическое занятие 5. Задание 1B. Расширение класса "Manager" с помощью поля "TeamSize". Добавлние конструктора в класс. Расчет бонуса в зависимости от размера команды.

---
+++ CSharpEducation.Practice/Practice5.Task1B/Employee.cs
@@ -0,0 +1,64 @@
+﻿using System;
+
+namespace Practice5.Task1B
+{
+    internal class Employee
+    {
+        string _name;
+        double _salary;
+        public string Name
+        {
+            get { return _name; }
+            set 
+            {
+                if ((value == "") || (value is null))
+                    throw new ArgumentNullException("Укажите имя сотрудника!");
+                _name = value;
+            }
+        }
+        public double Salary
+        {
+            get { return _salary; }
+            set 
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("Оклад не может быть отрицательным!");
+                _salary = value;
+            }
+        }
+        public Employee(string name, double salary)
+        {
+            Name = name;
+            Salary = salary;
+        }
+        public virtual double CalculateBonus()
+        {
+            return Salary * 0.1;
+        }
+    }
+    internal class Manager : Employee
+    {
+        int _teamsize;
+        public int TeamSize
+        {
+            get { return _teamsize; }
+            set 
+            {
+                if ((value < 0) || (value == 0))
+                    throw new ArgumentOutOfRangeException("Размер команды не может быть отрицательным и равным нулю!");
+                _teamsize = value;
+            }
+        }
+        public Manager(string name, double salary, int teamSize) : base(name, salary)
+        { 
+            Name = name;
+            Salary = salary;
+            TeamSize = teamSize;
+        }
+        public new double CalculateBonus()
+        {
+            int minSizeTeamForBonus = 5;
+            return (TeamSize > minSizeTeamForBonus) ? Salary * 0.25 : Salary * 0.2;
+        }
+    }
+}
---
+++ CSharpEducation.Practice/Practice5.Task1B/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Practice5.Task1B
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager mng1 = new Manager("Manager1", 50000, 5);
+            Manager mng2 = new Manager("Manager2", 50000, 6);
+            Console.WriteLine("Бонус менеджера при размере команды 1-5 сотрудников: " + mng1.CalculateBonus());
+            Console.WriteLine("Бонус менеджера при размере команды больше 5 сотрудников: " + mng2.CalculateBonus());
+            Console.ReadKey();
+        }
+    }
+}
