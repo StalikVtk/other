3f651a91b08664c32eb77fa8cbe5284791450dbc
StalikVtk <StalikVtk@yandex.ru>
2025-06-09 09:36:45 +0400

    Практическое занятие 5. Задание 1A. Создание класса "Employee" с полями "name", "salary". Возврат 10% от зараплаты с помощью метода "CalculateBonus". Создание производного класса "Manager". Возврат 20% от зараплаты с помощью переопределенного метода "CalculateBonus". Создание обьектов классов и вывод результата на экран.

---
+++ CSharpEducation.Practice/Practice5.Task1/Employee.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Practice5.Task1A
+{
+    internal class Employee
+    {
+        string _name;
+        double _salary;
+        public string Name
+        {
+            get { return _name; }
+            set 
+            {
+                if ((value == "") || (value is null))
+                    throw new ArgumentNullException("Укажите имя сотрудника!");
+                _name = value;
+            }
+        }
+        public double Salary
+        {
+            get { return _salary; }
+            set 
+            {
+                if (value < 0)
+                    throw new ArgumentOutOfRangeException("Оклад не может быть отрицательным!");
+                _salary = value;
+            }
+        }
+        public virtual double CalculateBonus()
+        {
+            return Salary * 0.1;
+        }
+    }
+    internal class Manager : Employee
+    {
+        public new double CalculateBonus()
+        {
+            return Salary * 0.2;
+        }
+    }
+}
---
+++ CSharpEducation.Practice/Practice5.Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace Practice5.Task1A
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Employee emp = new Employee();
+            emp.Name = "Рядовой сотрудник"; 
+            emp.Salary = 40000;
+            Manager mng = new Manager();
+            mng.Name = "Менеджер";
+            mng.Salary =  50000;
+
+            Console.WriteLine("Премия рядовго сотрудника: " + emp.CalculateBonus());
+            Console.WriteLine("Премия менеджера: " + mng.CalculateBonus());
+            Console.ReadKey();
+        }
+    }
+}
